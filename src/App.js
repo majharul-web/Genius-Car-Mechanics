59167f966a176d0350b366fdb80e9aeab7a9662b
Md Majharul Islam <80042446+majharul-web@users.noreply.github.com>
2021-10-13 15:18:36 +0600

    firebase deploy

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AuthProvider from './context/AuthProvider';
 import Booking from './Pages/Booking/Booking';
 import Home from './Pages/Home/Home/Home';
 import Login from './Pages/Login/Login/Login';
+import PrivateRoute from './Pages/Login/PrivateRoute/PrivateRoute';
 import NotFound from './Pages/NotFound/NotFound';
 import Header from './Pages/Shared/Header/Header';
 
@@ -25,9 +26,9 @@ function App() {
             <Route exact path='/login'>
               <Login></Login>
             </Route>
-            <Route path='/booking/:serviceID'>
+            <PrivateRoute path='/booking/:serviceID'>
               <Booking></Booking>
-            </Route>
+            </PrivateRoute>
             <Route path='*'>
               <NotFound></NotFound>
             </Route>
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -3,43 +3,52 @@ import { useEffect } from "react";
 import { useState } from "react";
 import initializeAuthentication from "../Pages/Login/Firebase/firebase.init";
 
-
 initializeAuthentication();
 const useFirebase = () => {
-    const [user, setUser] = useState({});
+  const [user, setUser] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
-    const auth = getAuth();
-    const provider = new GoogleAuthProvider();
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
 
-    // google sing in
-    const googleSingIn = () => {
-        signInWithPopup(auth, provider)
-            .then(result => {
-                console.log(result.user);
-            })
-    }
+  // google sing in
+  const googleSingIn = () => {
+      setIsLoading(true);
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        console.log(result.user);
+      })
+      .finally(() => setIsLoading(false));
+  };
 
-    // singOut
-    const logOut = () => {
-        signOut(auth).then(() => {
-            setUser({})
-        })
-    }
+  // singOut
+  const logOut = () => {
+    setIsLoading(true);
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .finally(() => setIsLoading(false));
+  };
 
-    useEffect(() => {
-        onAuthStateChanged(auth, (user) => {
-            if (user) {
-                setUser(user)
-            }
-        })
-    }, [])
+  useEffect(() => {
+    const unsubscribed = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser({});
+      }
+      setIsLoading(false);
+    });
+    return () => unsubscribed;
+  }, []);
 
-    return {
-        user,
-        googleSingIn,
-        logOut,
-
-    }
+  return {
+    user,
+    googleSingIn,
+    logOut,
+    isLoading
+  };
 };
 
 export default useFirebase;
